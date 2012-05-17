560bd41c8464645e80681615f51c1d820fbae611
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-05-17 16:59:13 +0200

    Factor code between VersionField and SpecField.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- src/semantic_version/django_fields.py
+++ src/semantic_version/django_fields.py
@@ -7,17 +7,34 @@ from django.utils.translation import ugettext_lazy as _
 from . import base
 
 
-class VersionField(models.CharField):
+class BaseSemVerField(models.CharField):
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 200)
+        super(BaseSemVerField, self).__init__(*args, **kwargs)
+
+    def get_prep_value(self, obj):
+        return str(obj)
+
+    def get_db_prep_value(self, value, connection, prepared=False):
+        if not prepared:
+            value = self.get_prep_value(value)
+        return value
+
+    def value_to_string(self, obj):
+        value = self.to_python(self._get_val_from_obj(obj))
+        return str(value)
+
+
+class VersionField(BaseSemVerField):
     default_error_messages = {
         'invalid': _(u"Enter a valid version number in X.Y.Z format."),
     }
     description = _(u"Version")
 
-    __metaclass__ = models.SubfieldBase
-
     def __init__(self, *args, **kwargs):
         self.partial = kwargs.pop('partial', False)
-        kwargs.setdefault('max_length', 40)
         super(VersionField, self).__init__(*args, **kwargs)
 
     def to_python(self, value):
@@ -28,47 +45,20 @@ class VersionField(models.CharField):
             return value
         return base.Version(value, partial=self.partial)
 
-    def get_prep_value(self, obj):
-        return str(obj)
 
-    def get_db_prep_value(self, value, connection, prepared=False):
-        if not prepared:
-            value = self.get_prep_value(value)
-        return value
-
-    def value_to_string(self, obj):
-        value = self.to_python(self._get_val_from_obj(obj))
-        return str(value)
-
-
-class SpecField(models.CharField):
+class SpecField(BaseSemVerField):
     default_error_messages = {
         'invalid': _(u"Enter a valid version number spec in ==X.Y.Z format."),
     }
     description = _(u"Version specification")
 
-    __metaclass__ = models.SubfieldBase
-
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault('max_length', 42)
-        return super(SpecField, self).__init__(*args, **kwargs)
-
     def to_python(self, value):
-        """Converts any value to a base.Version field."""
+        """Converts any value to a base.Spec field."""
         if value is None or value == '':
             return value
         if isinstance(value, base.Spec):
             return value
         return base.Spec(value)
 
-    def get_prep_value(self, obj):
-        return str(obj)
 
-    def get_db_prep_value(self, value, connection, prepared=False):
-        if not prepared:
-            value = self.get_prep_value(value)
-        return value
 
-    def value_to_string(self, obj):
-        value = self.to_python(self._get_val_from_obj(obj))
-        return str(value)
