ed74d6c409a4ea2a65dcd473563df06be727a9ef
Raphaël Barrois <raphael.barrois@polytechnique.org>
2014-03-08 21:14:24 +0100

    Update copyright

--- semantic_version/django_fields.py
+++ semantic_version/django_fields.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2012-2013 Raphaël Barrois
+# Copyright (c) 2012-2014 The python-semanticversion project
 # This code is distributed under the two-clause BSD License.
 
 from __future__ import unicode_literals
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2012-2013 Raphaël Barrois
+# Copyright (c) 2012-2014 The python-semanticversion project
 
 import os
 import re
