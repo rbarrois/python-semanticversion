66907b54e18091fef48e90e43f98e4ea1c96dccc
MinchinWeb <w_minchin@hotmail.com>
2015-08-31 12:47:50 -0600

    Update copyright year

--- semantic_version/django_fields.py
+++ semantic_version/django_fields.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2012-2014 The python-semanticversion project
+# Copyright (c) 2012-2015 The python-semanticversion project
 # This code is distributed under the two-clause BSD License.
 
 from __future__ import unicode_literals
